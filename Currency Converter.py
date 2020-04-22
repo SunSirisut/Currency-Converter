39c57df4320bb33553d75b1d9b6eec39796f645a
Pawitra Nopsuwan <pangpwt@gmail.com>
2020-04-22 14:51:28 +0700

    graph

--- Currency Converter.py
+++ Currency Converter.py
@@ -10,13 +10,19 @@ from autocomplete import *
 from urllib.request import urlopen, Request
 from PIL import Image, ImageTk
 import os
+import numpy as np
+import matplotlib.pyplot as plt
+from mpldatacursor import datacursor
+from datetime import datetime, timedelta
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from pandas import DataFrame
 if platform.system() == 'Darwin':
     from tkmacosx import Button
 
 root = Tk()
 
 root.title("Currency Exchanger")
-root.geometry("500x700")
+root.geometry("1000x400")
 root.grid_rowconfigure(1, weight=1)
 root.grid_columnconfigure(0, weight=1)
 
@@ -60,6 +66,26 @@ def get_full_path(file_name):
     return full_path
 
 
+def get_currency_by_range(f, t, r):
+    l_date = []
+    l_cur = []
+    
+    for i in range(r, 0, -1):
+        d = datetime.today() - timedelta(days = i)
+        l_date.append(d.strftime('%Y-%m-%d'))
+        
+    querystring = {"format" : "json", "to" : t, "from" : f, "amount" : "1"}
+    
+    for d in l_date:
+        print(d)
+        url = "https://currency-converter5.p.rapidapi.com/currency/historical/" + d
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        res = response.json()
+        l_cur.append(res['rates'][t]['rate'])
+        
+    return l_date, l_cur
+
+
 df_code_mapping = pd.read_csv(get_full_path('country_currency_mapping.csv'))
 
 def update_flag_from(cur_from):
@@ -98,6 +124,48 @@ def update_flag_to(cur_to):
         label_flag_to.configure(image = '')
 
 
+def show_graph(period):
+    curr_from = dd_from.get_value()
+    curr_to = dd_to.get_value()
+    
+    if curr_from != '' and curr_to != '':
+        for widget in frame_graph_2.winfo_children():
+            widget.destroy()
+
+        l_date, l_cur = get_currency_by_range(curr_from, curr_to, period)
+        y = [float(i) for i in l_cur]
+        # tmp_date = [int(d.split('-')[-1]) for d in l_date]
+        # x = range(min(tmp_date), max(tmp_date) + 1)
+        tmp_date = [d.split('-')[-2] + '/' + d.split('-')[-1] for d in l_date]
+        x = tmp_date
+        x_tmp = range(0, len(x))
+        print(x)
+        print(y)
+
+        fig = plt.Figure(dpi = 100)
+        fig, ax = plt.subplots()
+        fig.set_figheight(3.5)
+        fig.set_figwidth(5)
+        line = FigureCanvasTkAgg(fig, frame_graph_2)
+        line.get_tk_widget().pack()
+        ax.plot(x, y, color = 'blue', linewidth = 2.0, marker = 'o', markersize = 8)
+        ax.set_xticks(x)
+        ax.tick_params(axis = "x", labelsize = 8)
+        ax.tick_params(axis = "y", labelsize = 8)
+        ax.set_title('Exchange rate 1 ' + curr_from + ' to ' + curr_to)
+        ax.set_ylim(bottom = min(y) - (0.001 * min(y)), top = max(y) + (0.001 * max(y)))
+
+
+    def formatter(**kwargs):
+        dist = abs(np.array(x_tmp) - kwargs['x'])
+        # print(dist)
+        i = dist.argmin()
+        tmp = 'Date:' + str(l_date[i]) + '\n 1 ' + curr_from + ' = ' + str(str(y[i]) + ' ' + curr_to)
+        return tmp
+
+    datacursor(hover = True, formatter = formatter)
+
+
 def on_convert_click():
     curr_from = dd_from.get_value()
     curr_to = dd_to.get_value()
@@ -106,7 +174,8 @@ def on_convert_click():
     exchange_rate_data = requests.request("GET", exchange_rate_url, headers=headers, params=querystring).json()
     converted_amount = exchange_rate_data['rates'][curr_to]['rate_for_amount']
     label_result.configure(text=converted_amount)
-
+        
+    show_graph(3)
 
 def on_swap_click():
     tmp_from = dd_from.get_value()
@@ -126,13 +195,9 @@ def on_swap_click():
 frame_top = Frame(root, width = 500, height = 100)
 frame_top.grid(row = 0, column = 0, sticky = "NSEW")
 frame_top.grid_propagate(0)
+
 frame_mid = Frame(root, width = 500, height = 200)
 frame_mid.grid(row = 1, column = 0, sticky = "NSEW")
-frame_result = Frame(root, width = 500, height = 100)
-frame_result.grid(row = 2, column = 0, sticky = "NSEW")
-frame_graph = Frame(root, width = 500, height = 300,background = "Gray")
-frame_graph.grid(row = 3, column = 0, sticky = "NSEW")
-
 frame_mid_1 = Frame(frame_mid, width = 200, height=150)
 frame_mid_1.grid(row = 0, column = 0, sticky = "NSEW")
 frame_mid_2 = Frame(frame_mid, width = 100, height=150)
@@ -142,22 +207,33 @@ frame_mid_3.grid(row = 0, column = 2, sticky = "NSEW")
 frame_mid_4 = Frame(frame_mid, width = 200, height=50)
 frame_mid_4.grid(row = 2, column = 0, columnspan = 3, sticky = "NSEW")
 
-# FRAME TOP
-label_1 = Label(frame_top, text = 'Enter amount', font = ('Helvetica', 16))
-label_1.place(x = 250, y = 25, anchor="center")
+frame_result = Frame(root, width = 500, height = 100)
+frame_result.grid(row = 2, column = 0, sticky = "NSEW")
 
-entry_amount = Entry(frame_top, font = ('Helvetica', 20), justify = 'right')
-entry_amount.place(x = 250, y = 60, anchor="center")
+frame_graph = Frame(root, width = 500, height = 400)
+frame_graph.grid(row = 0, column = 1, rowspan = 3, padx = 5, pady = 5, sticky = "NSEW")
+frame_graph_1 = Frame(frame_graph, width = 500, height = 50)
+frame_graph_1.grid(row = 0, column = 0, sticky = "NSEW")
+frame_graph_2 = Frame(frame_graph, width = 500, height = 350)
+frame_graph_2.grid(row = 1, column = 0, sticky = "NSEW")
+
+
+# FRAME TOP
+label_1 = Label(frame_top, text = 'Enter amount', font = ('Helvetica', 18))
+label_1.place(x = 250, y = 30, anchor="center")
+
+entry_amount = Entry(frame_top, font = ('Helvetica', 20), justify = 'center')
+entry_amount.place(x = 250, y = 70, width = 450, height = 40, anchor="center")
 
 
 # FRAME MID
 ## MID 1
-label_2 = Label(frame_mid_1, text = 'From', font = ('Helvetica', 16))
+label_2 = Label(frame_mid_1, text = 'From', font = ('Helvetica', 18))
 label_2.place(x = 100, y = 20, anchor = 'center')
 
 code_from = StringVar()
-dd_from = Combobox_Autocomplete(frame_mid_1, width = 15, justify = 'center', font = ('Helvetica', 16), list_of_items = list_currency, textvariable = code_from)
-dd_from.place(x = 100, y = 60, anchor = 'center')
+dd_from = Combobox_Autocomplete(frame_mid_1, justify = 'center', font = ('Helvetica', 16), list_of_items = list_currency, textvariable = code_from)
+dd_from.place(x = 100, y = 60, width = 150, height = 40, anchor = 'center')
 dd_from.bind('<Return>', (lambda _: update_flag_from(dd_from.get_value())))
 dd_from.bind('<Button-1>', (lambda _: update_flag_from(dd_from.get_value())))
 
@@ -171,12 +247,12 @@ btn_swap = Button(frame_mid_2, image = img_swap,  width = 40, height = 40, comma
 btn_swap.place(x = 50, y = 60, anchor = 'center')
 
 ## MID 3
-label_3 = Label(frame_mid_3, text = 'To', font = ('Helvetica', 16))
+label_3 = Label(frame_mid_3, text = 'To', font = ('Helvetica', 18))
 label_3.place(x = 100, y = 20, anchor = 'center')
 
 code_to = StringVar()
-dd_to = Combobox_Autocomplete(frame_mid_3, width = 15, justify = 'center', font = ('Helvetica', 16), list_of_items = list_currency, textvariable = code_to)
-dd_to.place(x = 100, y = 60, anchor = 'center')
+dd_to = Combobox_Autocomplete(frame_mid_3, justify = 'center', font = ('Helvetica', 16), list_of_items = list_currency, textvariable = code_to)
+dd_to.place(x = 100, y = 60, width = 150, height = 40, anchor = 'center')
 dd_to.bind('<Return>', (lambda _: update_flag_to(dd_to.get_value())))
 dd_to.bind('<Button-1>', (lambda _: update_flag_to(dd_to.get_value())))
 
@@ -190,7 +266,33 @@ btn_convert.place( x = 250, y = 25, anchor = 'center')
 
 
 # RESULT
-label_result = Label(frame_result, text = '<Result>', font = ('Helvetica', 30))
+label_result = Label(frame_result, text = '', font = ('Helvetica', 36))
 label_result.place(x = 250, y = 50, anchor = 'center')
+
+
+## GRAPH
+# BUTTONS
+btn_3days = Button(frame_graph_1, text = '3 days', font = ('Helvetica', 12), command = lambda: show_graph(3))
+btn_3days.place( x = 80, y = 25, anchor = 'center')
+
+btn_1week = Button(frame_graph_1, text = '1 week', font = ('Helvetica', 12), command = lambda: show_graph(7))
+btn_1week.place( x = 250, y = 25, anchor = 'center')
+
+btn_1month = Button(frame_graph_1, text = '1 month', font = ('Helvetica', 12), command = lambda: show_graph(30))
+btn_1month.place( x = 420, y = 25, anchor = 'center')
+
+# GRAPH
+x = []
+y = []
+fig = plt.Figure(dpi = 100)
+fig, ax = plt.subplots()
+fig.set_figheight(3.5)
+fig.set_figwidth(5)
+line = FigureCanvasTkAgg(fig, frame_graph_2)
+line.get_tk_widget().pack()
+ax.plot(x, y, color = 'blue', linewidth = 2.0, marker = 'o', markersize = 8)
+ax.set_xticks(x)
+ax.tick_params(axis = "x", labelsize = 8)
+ax.tick_params(axis = "y", labelsize = 8)
 
 root.mainloop()
